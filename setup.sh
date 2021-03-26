6a44887b64778c2f0524d4b458a59d74aabc5a3e
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2021-03-26 14:28:30 +0000

    Cleanup and added touch wgetrc.

--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
 ### Git repo directory
-dir="/home/$USER/Projects/dotfiles"
+dir="$HOME/Projects/dotfiles"
 
-### Files
-([ -d "/home/$USER/.local/bin/scripts" ] && [ -d "/home/$USER/.local/bin/statusbar" ]) ||
-mkdir -p /home/$USER/.local/bin/{scripts,statusbar}
+### Create missing directories, if missing
+([ -d "$HOME/.local/bin/scripts" ] && [ -d "$HOME/.local/bin/statusbar" ]) ||
+mkdir -p $HOME/.local/bin/{scripts,statusbar}
 
+# List of all files to link
 files=$(find $dir -type f | grep -v -e ".git" -e "md" -e "setup")
 
+# Link every file
 for file in $files
 do
     link=$(echo $file | sed "s/\/Projects\/dotfiles//")
     ln -s -f $file $link
 done
+
+# Create wgetrc as .zprofile points to it
+mkdir -p $HOME/.config/wget
+touch $HOME/.config/wget/wgetrc
