14505661b0fc9efa36fa19a2303d62d1a6f91c1d
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2021-07-23 18:06:59 +0100

    Create link only if it does not exist.

--- setup.sh
+++ setup.sh
@@ -3,10 +3,23 @@
 # Git repo directory
 dir="$HOME/Projects/dotfiles"
 
+# Return the link path
+get_link() {
+	link=$(echo "$1" | sed "s/\/Projects\/dotfiles//")
+	echo "$link"
+}
+
+# Check if link already exists
+link_exists() {
+	[ -e "$link" ]
+}
+
+
 # Create missing directories, if missing
 [ -d "$HOME/.local/bin/scripts" ] && mkdir -p "$HOME"/.local/bin/scripts
 [ -d "$HOME/.local/bin/statusbar" ] && mkdir -p "$HOME"/.local/bin/statusbar
 [ -d "$HOME/.config/zsh" ] && mkdir -p "$HOME"/.config/zsh
+mkdir -p "$HOME"/.config/wget && touch "$HOME"/.config/wget/wgetrc
 
 # List of all files to link
 files=$(find "$dir" -type f | grep -v -e ".git" -e "md" -e "setup")
@@ -14,10 +27,6 @@ files=$(find "$dir" -type f | grep -v -e ".git" -e "md" -e "setup")
 # Link every file
 for file in $files
 do
-	link=$(echo "$file" | sed "s/\/Projects\/dotfiles//")
-	ln -s -f "$file" "$link"
+	link=$(get_link "$file")
+	! link_exists "$link" && ln -s -f "$file" "$link"
 done
-
-# Create wgetrc as .zprofile points to it
-mkdir -p "$HOME"/.config/wget
-touch "$HOME"/.config/wget/wgetrc
