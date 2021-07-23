1acb133bb9d823c904080d48475174d70537b87d
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2021-07-23 17:33:51 +0100

    Create missing zsh directory.

--- setup.sh
+++ setup.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-### Git repo directory
+# Git repo directory
 dir="$HOME/Projects/dotfiles"
 
-### Create missing directories, if missing
-([ -d "$HOME/.local/bin/scripts" ] && [ -d "$HOME/.local/bin/statusbar" ]) ||
-mkdir -p $HOME/.local/bin/{scripts,statusbar}
+# Create missing directories, if missing
+[ -d "$HOME/.local/bin/scripts" ] && mkdir -p "$HOME"/.local/bin/scripts
+[ -d "$HOME/.local/bin/statusbar" ] && mkdir -p "$HOME"/.local/bin/statusbar
+[ -d "$HOME/.config/zsh" ] && mkdir -p "$HOME"/.config/zsh
 
 # List of all files to link
-files=$(find $dir -type f | grep -v -e ".git" -e "md" -e "setup")
+files=$(find "$dir" -type f | grep -v -e ".git" -e "md" -e "setup")
 
 # Link every file
 for file in $files
 do
-	link=$(echo $file | sed "s/\/Projects\/dotfiles//")
-	ln -s -f $file $link
+	link=$(echo "$file" | sed "s/\/Projects\/dotfiles//")
+	ln -s -f "$file" "$link"
 done
 
 # Create wgetrc as .zprofile points to it
-mkdir -p $HOME/.config/wget
-touch $HOME/.config/wget/wgetrc
+mkdir -p "$HOME"/.config/wget
+touch "$HOME"/.config/wget/wgetrc
