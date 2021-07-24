129ce42e3fb2621bce6917c71a457c05c04207bd
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2021-07-24 18:12:06 +0100

    Automated directory creation.

--- setup.sh
+++ setup.sh
@@ -1,32 +1,39 @@
 #!/bin/sh
 
 # Git repo directory
-dir="$HOME/Projects/dotfiles"
+repo_dir="$HOME/Projects/dotfiles"
+
+# List of all files to link
+files=$(find "$repo_dir" -type f | grep -v -e ".git" -e "md" -e "setup")
+
 
 # Return the link path
 get_link() {
-	link=$(echo "$1" | sed "s/\/Projects\/dotfiles//")
-	echo "$link"
+	link=$(echo "$1" | sed "s|${repo_dir}||")
+	echo "$HOME""$link"
 }
 
 # Check if link already exists
 link_exists() {
-	[ -e "$link" ]
+	[ -e "$1" ]
+}
+
+# Return the parent directory
+get_parent_dir() {
+	echo "${1%/*}"
+}
+
+# Check if directory already exists
+dir_exists() {
+	[ -d "$1" ]
 }
 
 
-# Create missing directories, if missing
-[ -d "$HOME/.local/bin/scripts" ] && mkdir -p "$HOME"/.local/bin/scripts
-[ -d "$HOME/.local/bin/statusbar" ] && mkdir -p "$HOME"/.local/bin/statusbar
-[ -d "$HOME/.config/zsh" ] && mkdir -p "$HOME"/.config/zsh
-mkdir -p "$HOME"/.config/wget && touch "$HOME"/.config/wget/wgetrc
-
-# List of all files to link
-files=$(find "$dir" -type f | grep -v -e ".git" -e "md" -e "setup")
-
-# Link every file
+# Only link new files and create directories if missing
 for file in $files
 do
 	link=$(get_link "$file")
+	dir=$(get_parent_dir "$link")
+	! dir_exists "$dir" && mkdir -p "$dir"
 	! link_exists "$link" && ln -s -f "$file" "$link"
 done
