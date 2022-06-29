524901b9b1c82aa0663e4aa3d951238c04d45514
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-06-29 21:43:06 +0100

    Rework cmp menu and change copilot suggestion color

--- nvim/.config/nvim/lua/user/cmp.lua
+++ nvim/.config/nvim/lua/user/cmp.lua
@@ -1,54 +1,19 @@
--- Protected call to cmp
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-	print("cmp not installed!")
+	vim.notify("Failed to require cmp!")
 	return
 end
 
--- Protected call to lua snip
 local snip_status_ok, luasnip = pcall(require, "luasnip")
 if not snip_status_ok then
-	print("luasnip not installed!")
+	vim.notify("Failed to require luasnip!")
 	return
 end
 
--- Load snippets from vscode
 require("luasnip/loaders/from_vscode").lazy_load()
 
--- Helper for super tab functionality
-local check_backspace = function()
-	local col = vim.fn.col(".") - 1
-	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-end
-
--- Nerd Font icons
-local kind_icons = {
-	Text = "",
-	Method = "m",
-	Function = "",
-	Constructor = "",
-	Field = "",
-	Variable = "",
-	Class = "",
-	Interface = "",
-	Module = "",
-	Property = "",
-	Unit = "",
-	Value = "",
-	Enum = "",
-	Keyword = "",
-	Snippet = "",
-	Color = "",
-	File = "",
-	Reference = "",
-	Folder = "",
-	EnumMember = "",
-	Constant = "",
-	Struct = "",
-	Event = "",
-	Operator = "",
-	TypeParameter = "",
-}
+local icons = require("user.icons")
+local kind_icons = icons.kind
 
 cmp.setup({
 	snippet = {
@@ -59,28 +24,10 @@ cmp.setup({
 	mapping = {
 		["<C-k>"] = cmp.mapping.select_prev_item(),
 		["<C-j>"] = cmp.mapping.select_next_item(),
-		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		["<C-e>"] = cmp.mapping({
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.close(),
-		}),
-		["<C-y>"] = cmp.mapping.confirm({ select = true }),
-	},
-	formatting = {
-		fields = { "kind", "abbr", "menu" },
-		format = function(entry, vim_item)
-			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-			vim_item.menu = ({
-				nvim_lsp = "[LSP]",
-				nvim_lua = "[Lua]",
-				luasnip = "[Snippet]",
-				buffer = "[Buffer]",
-				path = "[Path]",
-			})[entry.source.name]
-			return vim_item
-		end,
+		["<C-h>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+		["<C-l>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+		["<C-y>"] = cmp.mapping.confirm({ select = false }),
+		["<C-e>"] = cmp.mapping.close(),
 	},
 	sources = {
 		{ name = "nvim_lsp" },
@@ -89,17 +36,26 @@ cmp.setup({
 		{ name = "buffer" },
 		{ name = "path" },
 	},
-	confirm_opts = {
-		behavior = cmp.ConfirmBehavior.Replace,
-		select = false,
+	formatting = {
+		format = function(entry, vim_item)
+			vim_item.kind = kind_icons[vim_item.kind]
+
+			vim_item.menu = ({
+				nvim_lsp = "",
+				nvim_lua = "",
+				luasnip = "",
+				buffer = "",
+				path = "",
+			})[entry.source.name]
+
+			return vim_item
+		end,
 	},
 	window = {
-		documentation = {
-			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-		},
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
 	},
 	experimental = {
-		ghost_text = true,
-		native_menu = false,
+		ghost_text = false,
 	},
 })
