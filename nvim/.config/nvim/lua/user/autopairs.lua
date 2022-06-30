216515d23cfde9236d4799271dd0a65da9e37237
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-06-30 19:45:12 +0100

    Rework autopairs

--- nvim/.config/nvim/lua/user/autopairs.lua
+++ nvim/.config/nvim/lua/user/autopairs.lua
@@ -1,20 +1,18 @@
--- Protected call to nvim-autopairs
 local status_ok, npairs = pcall(require, "nvim-autopairs")
 if not status_ok then
-	print("nvim-autopairs not installed!")
+	vim.notify("Failed to require nvim-autopairs!")
 	return
 end
 
 npairs.setup({
 	check_ts = true,
-	disable_filetype = { "TelescopePrompt" },
 })
 
 -- Better completion and autopairs
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-	print("cmp not installed!")
+	vim.notify("Failed to require cmp!")
 	return
 end
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
