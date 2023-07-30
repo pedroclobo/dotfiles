72a9b297dee3372353e529b3a1e39bc0c878d47b
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2023-07-30 21:44:48 +0100

    feat(xdg): add some default mime applications

--- modules/xdg/default.nix
+++ modules/xdg/default.nix
@@ -15,6 +15,26 @@ in {
 	config.hm = mkIf cfg.enable {
 		xdg = {
 			enable = true;
+			mimeApps = {
+				enable = true;
+				defaultApplications = {
+					"application/epub+zip" = "org.pwmt.zathura-pdf-mupdf.desktop";
+					"application/pdf" = "org.pwmt.zathura-pdf-mupdf.desktop";
+					"application/json" = "firefox.desktop";
+					"image/gif" = "sxiv.desktop";
+					"image/jpeg" = "sxiv.desktop";
+					"image/jpg" = "sxiv.desktop";
+					"image/png" = "sxiv.desktop";
+					"text/markdown" = "nvim.desktop";
+					"text/x-asm" = "nvim.desktop";
+					"text/x-c" = "nvim.desktop";
+					"text/x-fortran" = "nvim.desktop";
+					"text/x-java-source" = "nvim.desktop";
+					"text/x-pascal" = "nvim.desktop";
+					"text/x-python" = "nvim.desktop";
+					"text/x-shellscript" = "nvim.desktop";
+				};
+			};
 			userDirs = {
 				enable = true;
 				desktop = "${cfg.home}/.desktop";
